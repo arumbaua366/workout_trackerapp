67d275417b01b1dd7d9b5fd144cc64b174110bee
ar <56378858+arumbaua366@users.noreply.github.com>
2020-04-30 20:37:58 -0700

    update server

---
+++ controllers/controller.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+router.get(`/`, (req, res) => {
+  res.sendFile(path.join(__dirname, `../public/index.html`));
+});
+
+// exercise page
+router.get(`/exercise?`, (req, res) => {
+  res.sendFile(path.join(__dirname, `../public/exercise.html`));
+});
+
+// stats
+router.get(`/stats`, (req, res) => {
+  res.sendFile(path.join(__dirname, `../public/stats.html`));
+});
+
+module.exports = router;
